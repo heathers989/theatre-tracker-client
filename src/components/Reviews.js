13075f3b41354bf012b5ac276c866a0d2373ca99
Heather Spoelstra <hmspoelstra@gmail.com>
2020-05-21 22:37:09 -0400

    reviews page shows all fields

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -35,17 +35,41 @@ class Reviews extends React.Component {
   }
 
     render(){
-        console.log(this.state.currentMusical.reviews)
+        // console.log(this.state.currentMusical.reviews)
         return (
             <>
            <h1>Reviews for {this.state.currentMusical.name}</h1>
-           
-           {this.state.reviews.map(review => (
-               <div key={review.id}>
-                   <div>Review by: {review.reviewer_name}</div>
-               </div>
+
+           <table>
+        <tbody>
+        <tr>
+          <th>Review by:</th>
+          <th>Date of performance:</th> 
+          <th>Was most excited to see:</th> 
+          <th>Understudies seen (if any):</th>
+          <th>Rating from 1-5:</th>  
+          <th>Cast members seen at stage door (if any):</th> 
+          <th>Photos:</th> 
+          <th>Comments:</th> 
+         </tr> 
+         {this.state.reviews.map(review => (
+               <tr key={review.id}>
+                   <td>{review.reviewer_name}</td>
+                   <td>{review.date_of_show}</td>
+                   <td>{review.cast_to_see}</td>
+                   <td>{review.understudies_seen.map(understudy => (<div>{understudy}</div>))}</td>
+                   <td>{review.rating}</td>
+                   <td>{review.at_stagedoor.map(castMember => (<div>{castMember}</div>))}</td>
+                   <td>{review.photos}</td>
+                   <td id="comments">{review.comments}</td>
+
+               </tr>
 
            ))}
+    </tbody>
+    </table>
+           
+          
             <Link to="/musicals">
             <button onClick={() => this.props.toggleMusicals()}>Back to Musicals Index</button>
             </Link>
